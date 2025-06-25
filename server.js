ae751b7d55fc9dd3233c1156709647148046354d
M.K. Priyanka <121155062+PriyankaPriyadarshani1024@users.noreply.github.com>
2025-06-25 18:10:41 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/appointment', require('./routes/appointmentRoutes'));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
